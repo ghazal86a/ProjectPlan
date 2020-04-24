73921c01bea963f51134cac72296f27aadb43c52
Ghazal Amini <ghazal.86.a@gmail.com>
2020-04-24 16:31:16 +0100

    finish adding all components and styles

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,11 +1,11 @@
 import  React  from 'react';
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const SignedOutLinks = () => {
     return (
       <ul className="right">
-          <li><NavLink to="/">Sign Up</NavLink></li>
-          <li><NavLink to="/">Log In</NavLink></li>
+          <li><NavLink to="/signup">Sign Up</NavLink></li>
+          <li><NavLink to="/signin">Log In</NavLink></li>
       </ul>
     )
 }
